30b0f4564f19801a85456b0be181aa640f9ec558
Pratyush Kumar <pratyushkumar768@gmail.com>
2023-05-14 19:18:22 +0530

    initial commit

---
+++ app/src/main/java/com/example/birthdaywishes/BirthdayGreetings.kt
@@ -0,0 +1,11 @@
+package com.example.birthdaywishes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BirthdayGreetings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_birthday_greetings)
+    }
+}
